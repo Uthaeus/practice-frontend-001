c4d453e120a10c8144aa376c55a207540795e896
Uthaeus <romanlavery@gmail.com>
2023-07-20 19:57:58 -0600

    added blog nav to layout, created initial footer

---
+++ src/components/footers/blog-footer.js
@@ -0,0 +1,11 @@
+
+function BlogFooter() {
+
+    return (
+        <footer className="blog-footer">
+            <p className="blog-copyright">Copyright &#169; Homer J. 2023</p>
+        </footer>
+    );
+}
+
+export default BlogFooter;
